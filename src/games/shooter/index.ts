586cd50d88c215ca04570c13715972e23d7a2003
Alexander Karpov <alexander-karpov@outlook.com>
2023-05-11 00:21:40 +0300

    Управление камерой джойстиком

--- src/games/shooter/index.ts
+++ src/games/shooter/index.ts
@@ -13,7 +13,9 @@ import { Observable, type Observer } from '@babylonjs/core/Misc/observable';
 import { CameraInputsManager } from '@babylonjs/core/Cameras/cameraInputsManager';
 import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera';
 import { ScreenSizeBlock } from '@babylonjs/core/Materials/Node/Blocks/Fragment/screenSizeBlock';
+import { JoystickAxis, VirtualJoystick } from '@babylonjs/core/Misc/virtualJoystick';
 import { ZonedFreeCameraMouseInput } from './ZonedFreeCameraMouseInput';
+import { Vector2 } from 'three';
 
 const ss = new ScreenSizeBlock('ScreenSizeBlock01');
 
@@ -36,16 +38,36 @@ const scene = new Scene(engine);
 /**
  * Camera
  */
-const camera = new FreeCamera('camera1', new Vector3(0, 2, -10), scene);
-// const camera = new TargetCamera('camera1', new Vector3(0, 2, -10), scene);
+// const camera = new FreeCamera('camera1', new Vector3(0, 2, -10), scene);
+const camera = new TargetCamera('camera1', new Vector3(0, 2, -10), scene);
 
 camera.inertia = 0;
 
-camera.inputs.clear();
-camera.inputs.add(new ZonedFreeCameraMouseInput());
+// camera.inputs.clear();
+// camera.inputs.add(new ZonedFreeCameraMouseInput());
 
-// This attaches the camera to the canvas
-camera.attachControl(canvas, true);
+// // This attaches the camera to the canvas
+// camera.attachControl(canvas, true);
+
+// @ts-expect-error Переопределяем приватные методы
+class InvisibleVirtualJoystick extends VirtualJoystick {
+    private override _drawPuck() {}
+    private override _drawVirtualJoystick() {}
+
+    private override _onPointerDown(e: PointerEvent) {
+        this.deltaPosition.set(0, 0, 0);
+
+        // @ts-expect-error Вызов приватного метода
+        (super._onPointerDown as (e: PointerEvent) => void)(e);
+    }
+}
+
+const leftJoystick = new VirtualJoystick(true);
+const rightJoystick = new InvisibleVirtualJoystick(false, {
+    // puckSize: ,
+    // containerSize: 0,
+    limitToContainer: false,
+});
 
 // setInterval(() => {
 //     // console.log(camera.cameraRotation.x);
@@ -80,11 +102,29 @@ void (async () => {
         systemsRuntime.addSystem(system);
     }
 
+    const lastJoystickValue = new Vector2(0, 0);
+    const currentPos = new Vector2(0, 0);
+    rightJoystick.setJoystickSensibility(1);
+
     /**
      * Loop
      */
     engine.runRenderLoop(() => {
         systemsRuntime.update(engine.getDeltaTime() / 1000);
+
+        if (rightJoystick.pressed) {
+            lastJoystickValue.copy(currentPos);
+            currentPos.set(rightJoystick.deltaPosition.x, rightJoystick.deltaPosition.y);
+
+            const dy = rightJoystick.deltaPosition.y - lastJoystickValue.y;
+            const dx = rightJoystick.deltaPosition.x - lastJoystickValue.x;
+
+            camera.cameraRotation.set(-dy * engine.getDeltaTime(), dx * engine.getDeltaTime());
+        } else {
+            lastJoystickValue.set(0, 0);
+            currentPos.set(0, 0);
+        }
+
         scene.render();
     });
 })();
