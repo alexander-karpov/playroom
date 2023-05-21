f947249a58ce4d1461e5e563c04ed6e1fd693c2e
Alexander Karpov <alexander-karpov@outlook.com>
2023-05-21 03:20:27 +0300

    Меняет позицию камеры в home под женщин

--- src/games/home/index.ts
+++ src/games/home/index.ts
@@ -1,8 +1,8 @@
-import { start, world, scene, systemsRuntime, havok } from '~/game';
+import { start, world, scene, systemsRuntime, havok, playerCamera } from '~/game';
 import { LevelSystem } from '~/systems/LevelSystem';
 
-// // Приблизительный уровень глаз ребенка
-// playerCamera.position.y = 1.25;
+// Средний рост женщины минус примерно расстояние до глаз
+playerCamera.position.y = 1.65 - 0.15;
 
 systemsRuntime.addSystem(new LevelSystem(world, scene, havok));
 
